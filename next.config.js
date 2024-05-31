7b6d22bd46e9dc64a0643e371957fc2fa42d8270
eduardohv32 <eduardohv32@gmail.com>
2024-05-30 21:35:20 -0600

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: {
+    autoPrerender: false,
+  },
+  serverRuntimeConfig: {
+    port: 8080,
+  },
+  publicRuntimeConfig: {
+    port: 8080,
+  },
 }
 
 module.exports = nextConfig
